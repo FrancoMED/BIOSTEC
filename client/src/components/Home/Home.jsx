56ade46dbf4dce1406503a428d04bbcde3482c3a
FrancoMED <francoemedina@gmail.com>
2026-01-06 20:53:13 -0300

    add comnponent LandingPage

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar.jsx';
+import LandingPage from '../LandingPage/LandingPage.jsx';
 
 const Home = () => {
 	return (
 		<div className="Home">
 			<NavBar />
+			<LandingPage />
 		</div>
 	);
 };
