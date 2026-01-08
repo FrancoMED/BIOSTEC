633a455d3a3fff12a6c6bed99f5e9b58565c47c2
FrancoMED <francoemedina@gmail.com>
2026-01-08 15:16:27 -0300

    import Component Header

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Header/Header.jsx';
 import NavBar from '../NavBar/NavBar.jsx';
 import LandingPage from '../LandingPage/LandingPage.jsx';
 
 const Home = () => {
 	return (
 		<div className="Home">
+			<Header />
 			<NavBar />
 			<LandingPage />
 		</div>
