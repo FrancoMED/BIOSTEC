184fad45f739f4cffb66ad1babdac25de498baf2
FrancoMED <francoemedina@gmail.com>
2026-01-08 15:15:42 -0300

    add Component Header and css modules

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import LogoHeader from '../../Images/LogoTrans-header.jpg';
+import network from './netWork.js';
+
+function Header() {
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const cleanup = network(canvas);
+
+		// Cleanup al desmontar el componente
+		return cleanup;
+	}, []);
+
+	return (
+		<header class={styles.header}>
+			<canvas ref={canvasRef} />
+			<Link to="/">
+				<img src={LogoHeader} alt="Logo InfoSquad" class={styles.logo} />
+			</Link>
+		</header>
+	);
+}
+
+export default Header;
