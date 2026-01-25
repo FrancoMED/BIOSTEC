1494d68ec2b6d1b2c832572e70356162fb259857
FrancoMED <francoemedina@gmail.com>
2026-01-25 19:29:08 -0300

    fix import images in about and banner components

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../../components/Banner/Banner.jsx';
+import Banner from './components/Banner/Banner.jsx';
 import Features from '../../components/Features/Features.jsx';
 import Cards from './components/Cards/Cards.jsx';
 import Services from '../../components/Services/Services.jsx';
-import About from '../../components/About/About.jsx';
-import Contact from '../../components/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
 import styles from './Home.module.css';
 
 const Home = () => {
--- client/src/pages/Home/components/Banner/Banner.jsx
+++ client/src/pages/Home/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Banner.module.css';
-import home from '../../assets/Images/homeBiostec.jpg';
+import home from '../../../../assets/Images/homeBiostec.jpg';
 
 const Banner = () => {
 	return (
