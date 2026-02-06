e0fc428dfa518e27047d5cc3dcbffd97b3b067d3
FrancoMED <francoemedina@gmail.com>
2026-02-06 18:40:51 -0300

    remove Link img and change name ID

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import LogoHeader from "../../assets/Images/logoBiostec.png";
 import network from "./netWork.js";
 
 function Header() {
@@ -10,17 +8,13 @@ function Header() {
   useEffect(() => {
     const canvas = canvasRef.current;
     const cleanup = network(canvas);
-
     // Cleanup al desmontar el componente
     return cleanup;
   }, []);
 
   return (
-    <header id="inicio" class={styles.header}>
+    <header id="Inicio" class={styles.header}>
       <canvas ref={canvasRef} />
-      {/* <Link to="/">
-        <img src={LogoHeader} alt="Logo InfoSquad" class={styles.logo} />
-      </Link> */}
     </header>
   );
 }
